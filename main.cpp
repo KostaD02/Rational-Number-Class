1f2e62fd08b3864891b0258921e69fa35b1f304a
Kosta <68782786+KostaD02@users.noreply.github.com>
2021-05-01 17:10:02 +0400

    Creating main class

--- main.cpp
+++ main.cpp
@@ -2,7 +2,46 @@
 
 using namespace std;
 
+class RationalNumber{
+    private:
+        int m; //integer
+        unsigned int n; //denominator
+    public:
+        RationalNumber(int m=0,unsigned int n=1){
+            set(m, n);
+        }
+        void set(int m=0,unsigned int n=1){
+            this->m = m;
+            if(n!=0){
+                this->n = n;
+            }else{
+                cout << "Denominator can't be 0" << endl;
+                exit(0);
+            }
+        }
+        void Display(){
+            if(this->m==0){
+                cout << "0";
+            }else{
+                cout << this->m << "/" << this->n;
+            }
+        }
+        int getM(){
+            return this->m;
+        }
+        unsigned int getN(){
+            return this->n;
+        }
+};
+
 int main(int argc, char** argv) {
-    
+    RationalNumber A(2,3);
+    A.Display();
+    cout << endl;
+    RationalNumber B(0, 4);
+    B.Display();
+    cout << endl;
+    RationalNumber C(1, 0);
+    C.Display();
     return 0;
 }
